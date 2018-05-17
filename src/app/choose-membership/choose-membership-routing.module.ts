bffc459ec9490a664d01b944da3298df10af9a17
andrey0902 <andreypavlov1706@gmail.com>
2018-05-17 08:45:29 +0300

    add routing for choose membership module

--- src/app/choose-membership/choose-membership-routing.module.ts
+++ src/app/choose-membership/choose-membership-routing.module.ts
@@ -1,9 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { ChooseMembershipComponent } from './choose-membership.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: ChooseMembershipComponent
+      }
+    ])
+  ],
+  exports: [
+    RouterModule
   ],
   declarations: []
 })
--- src/app/choose-membership/choose-membership.module.ts
+++ src/app/choose-membership/choose-membership.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChooseMembershipComponent } from './choose-membership.component';
+import { ChooseMembershipRoutingModule } from './choose-membership-routing.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    ChooseMembershipRoutingModule,
   ],
   declarations: [ChooseMembershipComponent]
 })
